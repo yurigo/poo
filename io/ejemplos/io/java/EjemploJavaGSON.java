523709ad969d92f56c28226c18a254b990cd6762
La Salle BCN <lspublic@salle.salleURL.edu>
2023-12-05 19:31:06 +0100

    add ejemplojavaGSON

---
+++ io/ejemplos/io/java/EjemploJavaGSON.java
@@ -0,0 +1,45 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+
+        // usuarios es un map y en el fromJson hay que
+        // utilizar Map<Integer, User> de un modo especial:
+
+        /*
+         {
+                "1": {
+                    "login": "yuri",
+                    "password": "yuri"
+                },
+                "2": {
+                    "login": "Alice",
+                    "password": "alice"
+                }
+            }
+         */
+
+        String filepath = "./usuarios.json";
+
+        File jsonfile = new File(filepath);
+        try {
+            FileReader fileReader = new FileReader(jsonfile);
+            Map<Integer, User> usuarios = gson.fromJson(fileReader , new TypeToken<Map<Integer, User>>() {}.getType());
+            
+            System.out.println(usuarios.get(2).getLogin());
+            
+            for (User usuario: usuarios.values()) {
+                System.out.println(usuario.getLogin() + " " + usuario.getPassword());
+            }
+            
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
